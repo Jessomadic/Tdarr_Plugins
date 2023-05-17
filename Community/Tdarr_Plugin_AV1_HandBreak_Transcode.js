1cdbbac79a6b72ddbee237596ea9607ee8be7dea
Jessomadic <Jessecopas@gmail.com>
2023-05-17 12:16:00 -0400

    Fixed Bitrate assigning failure
    Changed how plugin detects bitrate. As it was working for some codecs and not for others depending on how it was encoded I changed it from reading the bitrate of the file to calculating the bitrate.
    Tested on Res 480->1080
    Mpeg4,H264,Hvec

--- Community/Tdarr_Plugin_AV1_HandBreak_Transcode.js
+++ Community/Tdarr_Plugin_AV1_HandBreak_Transcode.js
@@ -1,72 +1,72 @@
 /* eslint no-plusplus: ["error", { "allowForLoopAfterthoughts": true }] */
 /* eslint-disable no-restricted-globals */
 const details = () => ({
-    id: "Tdarr_Plugin_AV1_HandBreak_Transcode",
-    Name: "AV1 HandBreak Transcoder",
-    Type: "Video",
-    Operation: "Transcode",
-    Description: "Transcodes to AV1 at the selected Bitrate. This is best used with Remux Files.",          
-    Version: "2.1.2",
-    Tags: "HandBrake,AV1",
-    Author:"Jessomadic",
-  
-    Inputs: [
-      {
-        name: 'BitRate',
-        type: 'string',
-        defaultValue: '4000',
-        inputUI: {
-          type: 'text',
-        },
-        tooltip: `
+  id: 'Tdarr_Plugin_AV1_HandBreak_Transcode',
+  Name: 'AV1 HandBreak Transcoder',
+  Type: 'Video',
+  Operation: 'Transcode',
+  Description: 'Transcodes to AV1 at the selected Bitrate. This is best used with Remux Files.',
+  Version: '2.1.2',
+  Tags: 'HandBrake,AV1',
+  Author: 'Jessomadic',
+
+  Inputs: [
+    {
+      name: 'BitRate',
+      type: 'string',
+      defaultValue: '4000',
+      inputUI: {
+        type: 'text',
+      },
+      tooltip: `
         ~ Requested Bitrate ~ \\n
         Put in the Bitrate you want to process to in Kbps. For example 4000Kbps is 4Mbps. 
         Remember that AV1 is much more efficient with data than h264/5
         I noticed that around 4Mbps is the sweet spot for 1080p from remux. \\n\\n`,
+    },
+    {
+      name: 'ResolutionSelection',
+      type: 'string',
+      defaultValue: '1080p',
+      inputUI: {
+        type: 'dropdown',
+        options: [
+          '8KUHD',
+          '4KUHD',
+          '1080p',
+          '720p',
+          '480p',
+        ],
       },
-      {
-        name: 'ResolutionSelection',
-        type: 'string',
-        defaultValue: '1080p',
-        inputUI: {
-          type: 'dropdown',
-          options: [
-            '8KUHD',
-            '4KUHD',
-            '1080p',
-            '720p',
-            '480p',
-          ],
-        },
-        tooltip: `Any Resolution larger than this will become this Resolution `,
+      tooltip: 'Any Resolution larger than this will become this Resolution ',
+    },
+    {
+      name: 'Container',
+      type: 'string',
+      defaultValue: 'mkv',
+      inputUI: {
+        type: 'dropdown',
+        options: [
+          'mp4',
+          'mkv',
+        ],
       },
-      {
-        name: 'Container',
-        type: 'string',
-        defaultValue: 'mkv',
-        inputUI: {
-          type: 'dropdown',
-          options: [
-            'mp4',
-            'mkv',
-          ],
-        },
-        tooltip: ` Container Type \\n\\n
+      tooltip: ` Container Type \\n\\n
           mkv or mp4.\\n`,
-      },
-    ],
-  
-  });
-  const MediaInfo = {
-    videoHeight: '',
-    videoWidth: '',
-    videoFPS: '',
-    videoBR: '',
-    videoBitDepth: '',
-    overallBR: '',
-  }; // var MediaInfo
-  
-  // Easier for our functions if response has global scope.
+    },
+  ],
+
+});
+const MediaInfo = {
+  videoHeight: '',
+  videoWidth: '',
+  videoFPS: '',
+  videoBR: '',
+  videoBitDepth: '',
+  overallBR: '',
+}; // var MediaInfo
+
+// Easier for our functions if response has global scope.
 const response = {
   processFile: false,
   preset: '',
@@ -83,12 +83,12 @@ function getMediaInfo(file) {
 
   for (let i = 0; i < file.ffProbeData.streams.length; i += 1) {
     const strstreamType = file.ffProbeData.streams[i].codec_type.toLowerCase();
-        // Looking For Video
+    // Looking For Video
     // Check if stream is a video.
     if (videoIdx === -1 && strstreamType === 'video') {
       videoIdx = i;
-// get video streams resolution
-      MediaInfo.videoResolution = file.ffProbeData.streams[i].height + 'x' + file.ffProbeData.streams[i].width;
+      // get video streams resolution
+      MediaInfo.videoResolution = `${file.ffProbeData.streams[i].height}x${file.ffProbeData.streams[i].width}`;
       MediaInfo.videoHeight = Number(file.ffProbeData.streams[i].height);
       MediaInfo.videoWidth = Number(file.ffProbeData.streams[i].width);
       MediaInfo.videoFPS = Number(file.mediaInfo.track[i + 1].FrameRate);
@@ -99,72 +99,78 @@ function getMediaInfo(file) {
   MediaInfo.overallBR = file.mediaInfo.track[0].OverallBitRate;
 } // end  getMediaInfo()
 
+// define resolution order from ResolutionSelection from biggest to smallest
+const resolutionOrder = ['8KUHD', '4KUHD', '1080p', '720p', '480p'];
 
-//define resolution order from ResolutionSelection from biggest to smallest
-const resolutionOrder = [ '8KUHD','4KUHD','1080p','720p','480p'];
+// define the width and height of each resolution from the resolution order
+const resolutionsdimensions = {
+  '8KUHD': '--width 7680 --height 4320',
+  '4KUHD': '--width 3840 --height 2160',
+  '1080p': '--width 1920 --height 1080',
+  '720p': '--width 1280 --height 720',
+  '480p': '--width 640 --height 480',
+};
 
-// define the width and height of each resolution from the resolution order 
-  const resolutionsdimensions = {
-    '8KUHD' : "--width 7680 --height 4320",
-    '4KUHD' : "--width 3840 --height 2160",
-    '1080p' : "--width 1920 --height 1080",
-    '720p' : "--width 1280 --height 720",
-    '480p' : "--width 640 --height 480",
-  };
-    
+// eslint-disable-next-line no-unused-vars
+const plugin = (file, librarySettings, inputs) => {
   // eslint-disable-next-line no-unused-vars
-  const plugin = (file, librarySettings, inputs) => {
-    const importFresh = require('import-fresh');
-    const library = importFresh('../methods/library.js');
-  
-    // Get the selected resolution from the 'ResolutionSelection' variable
-    const selectedResolution = inputs.ResolutionSelection;
-  
-    getMediaInfo(file);
+  const importFresh = require('import-fresh');
+  // eslint-disable-next-line no-unused-vars
+  const library = importFresh('../methods/library.js');
+
+  // Get the selected resolution from the 'ResolutionSelection' variable
+  const selectedResolution = inputs.ResolutionSelection;
+
+  getMediaInfo(file);
   // use mediainfo to match height and width to a resolution on resolutiondimensions
   let dimensions = resolutionsdimensions[selectedResolution];
   // if the file is smaller than the selected resolution then use the file resolution
-  if(MediaInfo.videoHeight < dimensions.split(" ")[3] || MediaInfo.videoWidth < dimensions.split(" ")[1]){
-    dimensions = "--width " + MediaInfo.videoWidth + " --height " + MediaInfo.videoHeight;
+  if (MediaInfo.videoHeight < dimensions.split(' ')[3] || MediaInfo.videoWidth < dimensions.split(' ')[1]) {
+    dimensions = `--width ${MediaInfo.videoWidth} --height ${MediaInfo.videoHeight}`;
   }
+
   // if the file is larger than the selected resolution then use the selected resolution
-  else{
+  else {
     // loop through the resolution order
     for (let i = 0; i < resolutionOrder.length; i += 1) {
       // if the selected resolution is the same as the current resolution in the loop
-      if(selectedResolution === resolutionOrder[i]){
+      if (selectedResolution === resolutionOrder[i]) {
         // break the loop
         break;
       }
     }
-  }            
-    //check the files bitrate and if it is smaller than the selected bitrate then use the file bitrate in kbps
-    getMediaInfo(file);
-    BR = MediaInfo.videoBR/1000;
-    if(BR < inputs.BitRate){
-      inputs.BitRate = BR;
-    }
-    // if the file is larger than the selected bitrate then use the selected bitrate 
-    else{
-      inputs.BitRate = inputs.BitRate;
-    }
-  
-    
-        response.infoLog += ""
-        if((true) || file.forceProcessing === true){
-            response.preset = "--encoder svt_av1 -b " + inputs.BitRate + ' -r 24 -E aac -B 160 -R Auto -6 dpl2 -f ' + inputs.Container + ' --optimize ' + dimensions + ' --crop 0:0:0:0';
-            response.container = '.' + inputs.Container;
-            response.handbrakeMode = true
-            response.ffmpegMode = false
-            response.processFile = true
-            response.infoLog +=  'File is being transcoded at ' + inputs.BitRate + ' Kbps to ' + dimensions + ' as ' + inputs.Container + ' \n'
-            return response
-           }else{
-            response.infoLog += "File is being transcoded using custom arguments \n"
-            return response
-           }
-      }
-    
+  }
+
+  // Calculate the bitrate for the media file using the current width and height and frame rate of the file
+  // If the calulated bitrate is over 150000 assume it's in bits and convert to kbps
+  let calculatedBitrate = Math.round((MediaInfo.videoHeight * MediaInfo.videoWidth * MediaInfo.videoFPS * 0.07) / 1000);
+  if (calculatedBitrate > 150000) {
+    calculatedBitrate = Math.round(calculatedBitrate / 1000);
+  }
+  // if the calculated bitrate is less than the selected bitrate then use the calculated bitrate
+  if (calculatedBitrate < inputs.BitRate) {
+    // eslint-disable-next-line no-param-reassign
+    inputs.BitRate = calculatedBitrate;
+  }
+  // If Bitrate is larger than the selected bitrate then use the selected bitrate
+  else {
+    // eslint-disable-next-line no-self-assign
+    inputs.BitRate = inputs.BitRate;
+  }
+
+  response.infoLog += '';
+  if ((true) || file.forceProcessing === true) {
+    response.preset = `--encoder svt_av1 -b ${inputs.BitRate} -r 24 -E aac -B 160 -R Auto -6 dpl2 -f ${inputs.Container} --optimize ${dimensions} --crop 0:0:0:0`;
+    response.container = `.${inputs.Container}`;
+    response.handbrakeMode = true;
+    response.ffmpegMode = false;
+    response.processFile = true;
+    response.infoLog += `File is being transcoded at ${inputs.BitRate} Kbps to ${dimensions} as ${inputs.Container} \n`;
+    return response;
+  }
+  response.infoLog += 'File is being transcoded using custom arguments \n';
+  return response;
+};
 
 module.exports.details = details;
 module.exports.plugin = plugin;
